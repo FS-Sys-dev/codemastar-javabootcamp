7842ebdb4ce5030cb2d36c073f1fde1370fbcec8
FS-Sys-dev <fssysnpd@freestyles.jp>
2024-10-02 16:50:14 +0900

    誤記修正

--- 02.entry/16.text-input/src/BadInputRecieverAnother.java
+++ 02.entry/16.text-input/src/BadInputRecieverAnother.java
@@ -4,13 +4,13 @@ public class BadInputRecieverAnother {
     public static void main(String[] args) {
 
         Scanner stdin = new Scanner(System.in);
-        System.out.print("文字列を入力してください: ");
+        System.out.print("数字を入力してください: ");
 
         String strNumber = stdin.nextLine();
 
         try {
             int intNumber = Integer.parseInt(strNumber);
-            System.out.printf("入力された文字列は %d です.", intNumber);
+            System.out.printf("入力された数字は %d です.", intNumber);
 
         } catch (NumberFormatException e) {
             System.out.println("数字以外の文字が入力されました.");
